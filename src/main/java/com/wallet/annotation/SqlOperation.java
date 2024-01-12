5e3fd29e6a8f78d2809d313520c478f079972d30
Fiantso Harena <fiantsorav@gmail.com>
2024-01-12 17:13:04 +0300

    fix: separate save method into two method distinct

--- src/main/java/com/wallet/annotation/SqlOperation.java
+++ src/main/java/com/wallet/annotation/SqlOperation.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SqlOperation {
-  String type();
+  Type type();
 
   enum Type {
     SELECT, INSERT, UPDATE, DELETE
--- src/main/java/com/wallet/repository/CrudOperations.java
+++ src/main/java/com/wallet/repository/CrudOperations.java
@@ -1,19 +1,32 @@
 package com.wallet.repository;
 
+import com.wallet.annotation.SqlOperation;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.List;
 
 public interface CrudOperations<T> {
+  @SqlOperation(type = SqlOperation.Type.SELECT)
   T findById(Long toFind);
 
+  @SqlOperation(type = SqlOperation.Type.SELECT)
   List<T> findAll();
 
+  @SqlOperation(type = SqlOperation.Type.INSERT)
   List<T> saveAll(List<T> toSave);
 
+  @SqlOperation(type = SqlOperation.Type.UPDATE)
+  List<T> updateAll(List<T> toUpdate);
+
+  @SqlOperation(type = SqlOperation.Type.INSERT)
   T save(T toSave);
 
+  @SqlOperation(type = SqlOperation.Type.UPDATE)
+  T update(T toUpdate);
+
+  @SqlOperation(type = SqlOperation.Type.DELETE)
   void delete(T toDelete);
 
   void closeResources(Connection connection, PreparedStatement statement, ResultSet resultSet);
