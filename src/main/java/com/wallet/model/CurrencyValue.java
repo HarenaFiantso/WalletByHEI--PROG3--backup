c073530b236045c3c432a2d7b424377ad8932100
Fiantso Harena <fiantsorav@gmail.com>
2024-01-13 10:40:08 +0300

    refactor: optimize the readability of the Transaction entity

--- src/main/java/com/wallet/model/CurrencyValue.java
+++ src/main/java/com/wallet/model/CurrencyValue.java
@@ -2,28 +2,40 @@ package com.wallet.model;
 
 import com.wallet.annotations.DatabaseField;
 import com.wallet.annotations.DatabaseTable;
+
 import java.time.LocalDateTime;
+
+import com.wallet.annotations.type.ColumnType;
+import com.wallet.annotations.type.GenerativeValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+/**
+ * Represents a user account in the system.
+ *
+ * @see DatabaseTable: Used to specify the database table associated with the current entity
+ * @see DatabaseField: Used to specify the database field associated with the current table
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
 @DatabaseTable(name = "currency_value")
 public class CurrencyValue {
-  @DatabaseField(name = "currency_value_id")
+
+  @DatabaseField(name = "currency_value_id", identity = true, generative = GenerativeValue.SEQUENCE)
   private Long currencyValueId;
 
-  @DatabaseField(name = "currency_value_date")
+  @DatabaseField(name = "currency_value_date", columnType = ColumnType.DATE, required = true)
   private LocalDateTime currencyValueDate;
 
-  @DatabaseField(name = "exchange_rate")
+  @DatabaseField(name = "exchange_rate", columnType = ColumnType.DOUBLE, required = true)
   private Double exchangeRate;
 
-  @DatabaseField(name = "destination_currency_id")
+  @DatabaseField(name = "destination_currency_id", columnType = ColumnType.INT, references = true, required = true)
   private int destinationCurrencyId;
 
-  @DatabaseField(name = "source_currency_id")
+  @DatabaseField(name = "source_currency_id", columnType = ColumnType.INT, references = true, required = true)
   private int sourceCurrencyId;
 }
--- src/main/java/com/wallet/model/Transaction.java
+++ src/main/java/com/wallet/model/Transaction.java
@@ -2,35 +2,45 @@ package com.wallet.model;
 
 import com.wallet.annotations.DatabaseField;
 import com.wallet.annotations.DatabaseTable;
+import com.wallet.annotations.type.ColumnType;
+import com.wallet.annotations.type.GenerativeValue;
 import com.wallet.model.type.TransactionType;
 import java.sql.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+/**
+ * Represents a user account in the system.
+ *
+ * @see DatabaseTable: Used to specify the database table associated with the current entity
+ * @see DatabaseField: Used to specify the database field associated with the current table
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
 @DatabaseTable(name = "transaction")
 public class Transaction {
-  @DatabaseField(name = "transaction_id")
+
+  @DatabaseField(name = "transaction_id", identity = true, generative = GenerativeValue.SEQUENCE)
   private Long transactionId;
 
-  @DatabaseField(name = "amount")
+  @DatabaseField(name = "amount", columnType = ColumnType.DOUBLE, required = true)
   private Double amount;
 
-  @DatabaseField(name = "label")
+  @DatabaseField(name = "label", columnType = ColumnType.VARCHAR, required = true)
   private String label;
 
-  @DatabaseField(name = "transaction_date")
+  @DatabaseField(name = "transaction_date", columnType = ColumnType.TIMESTAMP)
   private Timestamp transactionDate;
 
-  @DatabaseField(name = "transaction_type")
+  @DatabaseField(name = "transaction_type", columnType = ColumnType.TEXT, required = true)
   private TransactionType transactionType;
 
-  @DatabaseField(name = "account_id")
+  @DatabaseField(name = "account_id", columnType = ColumnType.INT, references = true, required = true)
   private int accountId;
 
-  @DatabaseField(name = "category_id")
+  @DatabaseField(name = "category_id", columnType = ColumnType.INT, references = true, required = true)
   private int categoryId;
 }
