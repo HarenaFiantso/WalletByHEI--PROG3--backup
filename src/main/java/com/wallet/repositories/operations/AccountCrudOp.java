95d62b09b3275e5d707484ae13bfbcc7700683d5
Fiantso Harena <fiantsorav@gmail.com>
2024-01-13 18:48:05 +0300

    fix: use attribut to store the account entity field

--- src/main/java/com/wallet/repositories/operations/AccountCrudOp.java
+++ src/main/java/com/wallet/repositories/operations/AccountCrudOp.java
@@ -5,16 +5,20 @@ import com.wallet.repositories.implementations.CrudMakerParams;
 import com.wallet.repositories.implementations.CrudOperationsImpl;
 
 public class AccountCrudOp extends CrudOperationsImpl<Account> {
+  private static final String ACCOUNT_NAME = "account_name";
+  private static final String ACCOUNT_TYPE = "account_type";
+  private static final String CURRENCY_ID = "currency_id";
+  private static final String ACCOUNT_ID = "account_id";
 
   public AccountCrudOp() {
     super(
         CrudMakerParams
             .builder()
             .entityClass(Account.class)
-            .createColumnSet(new String[]{"account_name", "account_type", "currency_id"})
-            .updateByColumn("account_id")
-            .updatableColumns(new String[]{"account_name"})
-            .deleteByAColumn("account_id")
+            .createColumnSet(new String[]{ACCOUNT_NAME, ACCOUNT_TYPE, CURRENCY_ID})
+            .updateByColumn(ACCOUNT_ID)
+            .updatableColumns(new String[]{ACCOUNT_NAME})
+            .deleteByAColumn(ACCOUNT_ID)
             .build()
     );
   }
